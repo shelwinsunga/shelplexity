fc2fe608c1f5f1180b36a5e435363d525f1978dc
Shelwin <shelwinsunga@gmail.com>
2024-07-29 12:58:06 -0500

    feat: styling shelplexity

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Snail } from 'lucide-react';
 export default function Page() {
   return (
     <>
-      <div className="container mx-auto px-4 py-8 relative flex flex-col items-center justify-center h-screen">
+      <div className="container mx-auto -mt-16 px-4 py-8 relative flex flex-col items-center justify-center h-screen">
         <div className="flex items-center mb-8">
           <Snail className="w-12 h-12 mr-4" />
           <h1 className="text-3xl font-semibold">Shelplexity</h1>

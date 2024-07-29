56c28fe6c517c1edb966be1750499e6aea825516
Shelwin <shelwinsunga@gmail.com>
2024-07-29 08:20:12 -0500

    feat: update handleQuery

---
+++ actions/saveQuery.ts
@@ -0,0 +1,16 @@
+'use server'
+import { kv } from "@vercel/kv";
+type QueryStatus = 'pending' | 'complete' | 'error';
+
+export async function saveFrontendContext(frontendContextId: string, query: string, queryStatus: QueryStatus) {
+  try {
+    await kv.hmset(`frontend-context-id:${frontendContextId}`, {
+      query: query,
+      status: queryStatus
+    });
+    console.log(`Frontend context saved: ID - ${frontendContextId}, Query - ${query}`);
+  } catch (e) {
+    console.error(`Failed to save frontend context: ID - ${frontendContextId}, Query - ${query}`);
+    throw new Error('Failed to save frontend context')
+  }
+}
--- contexts/FrontendContext.tsx
+++ contexts/FrontendContext.tsx
@@ -8,60 +8,67 @@ import { useEffect } from 'react';
 import { useAIState } from 'ai/rsc';
 import { useSearchParams } from 'next/navigation';
 import { generateHash } from '@/lib/utils';
-
+import { v4 as uuidv4 } from 'uuid';
+import { saveFrontendContext } from '@/actions/saveQuery';
 
 interface FrontendContextType {
   query: string;
-  queryId: string;
   handleQuery: (query: string) => void;
   setQuery: (query: string) => void;
-  setQueryId: (queryId: string) => void;
+  frontendContextId: string | null;
+  setFrontendContextId: (frontendContextId: string | null) => void;
 }
 
 const FrontendContext = createContext<FrontendContextType | undefined>(undefined);
 
 export function FrontendProvider({ children }: { children: React.ReactNode }) {
   const [query, setQuery] = useState('');
-  const [queryId, setQueryId] = useState('');
+  const [frontendContextId, setFrontendContextId] = useState<string | null>(null);
+  const [queryStatus, setQueryStatus] = useState<'pending' | 'complete' | 'error'>('pending');
   const [conversation, setConversation] = useUIState();
   const { continueConversation } = useActions();
   const [AIState, setAIState] = useAIState();
   const searchParams = useSearchParams();
   const router = useRouter();
 
-
   const handleQuery = async (newQuery: string) => {
+    const newFrontendContextId = uuidv4();
+    const queryStatus = 'pending';
+    setFrontendContextId(newFrontendContextId);
     setQuery(newQuery);
-    router.push(`/search?q=${encodeURIComponent(newQuery)}`);
+    setQueryStatus(queryStatus);
+    saveFrontendContext(newFrontendContextId, newQuery, queryStatus);
+    router.push(`/search?q=${queryStatus}&newFrontendContextUUID=${newFrontendContextId}`);
 
-    setConversation((currentConversation: ClientMessage[]) => [
-      ...currentConversation,
-      { id: generateId(), role: 'user', display: newQuery },
-    ]);
+    // setConversation((currentConversation: ClientMessage[]) => [
+    //   ...currentConversation,
+    //   { id: generateId(), role: 'user', display: newQuery },
+    // ]);
 
-    const message = await continueConversation(newQuery);
+    // const message = await continueConversation(newQuery);
 
-    setConversation((currentConversation: ClientMessage[]) => [
-      ...currentConversation,
-      message,
-    ]);
+    // setConversation((currentConversation: ClientMessage[]) => [
+    //   ...currentConversation,
+    //   message,
+    // ]);
   };
 
-  useEffect(() => {
-    if (searchParams.get('q') === 'pending') {
-      const slug = query.toLowerCase().replace(/\s+/g, '-');
-      const hash = generateHash();
-      const newPath = `/search/${slug}-${hash}`;
-      window.history.replaceState(null, '', newPath);
-    }
-  }, [AIState, router]);
+  // useEffect(() => {
+  //   if (searchParams.get('q') === 'pending') {
+  //     const slug = query.toLowerCase().replace(/\s+/g, '-');
+  //     const hash = generateHash();
+  //     const newPath = `/search/${slug}-${hash}`;
+  //     window.history.replaceState(null, '', newPath);
+  //   }
+  // }, [AIState, router]);
 
   return (
-    <FrontendContext.Provider value={{ query, queryId, handleQuery, setQuery, setQueryId }}>
+    <FrontendContext.Provider value={{ query, handleQuery, setQuery, frontendContextId, setFrontendContextId }}>
       {children}
     </FrontendContext.Provider>
   );
 }
+
 export function useFrontend() {
   const context = useContext(FrontendContext);
   if (context === undefined) {

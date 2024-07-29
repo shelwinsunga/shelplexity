9219c746977b24a11af013bebfc5b15f4d1d6a1d
Shelwin <shelwinsunga@gmail.com>
2024-07-29 12:57:09 -0500

    feat: get brave api key

--- actions/searchWeb.ts
+++ actions/searchWeb.ts
@@ -4,6 +4,8 @@ import fs from 'fs/promises'
 import path from 'path'
 import { cache } from 'react'
 
+const BRAVE_API_KEY = process.env.BRAVE_API_KEY
+
 export const searchWeb = cache(async (query: string | null): Promise<any[]> => {
     if (!query) {
         return []
@@ -20,7 +22,6 @@ export const searchWeb = cache(async (query: string | null): Promise<any[]> => {
 })
 
 // Commented out code for Brave API integration
-// const BRAVE_API_KEY = 'BSAvvxzei7ZdGlkfxbAbsyVi0MCNrpK';
 // ... [rest of the commented out code] ...
 
-// curl -s --compressed "https://api.search.brave.com/res/v1/web/search?q=brave+search" -H "Accept: application/json" -H "Accept-Encoding: gzip" -H "X-Subscription-Token: BSAvvxzei7ZdGlkfxbAbsyVi0MCNrpK"
+// curl -s --compressed "https://api.search.brave.com/res/v1/web/search?q=brave+search" -H "Accept: application/json" -H "Accept-Encoding: gzip" -H "X-Subscription-Token: ${BRAVE_API_KEY}"

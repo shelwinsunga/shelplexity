e418f932e29a711cb373da27bf28666fc7fd3ea7
Shelwin <shelwinsunga@gmail.com>
2024-07-28 21:33:34 -0500

    feat: more layout

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,13 +1,8 @@
-'use client';
-import { SearchArea } from '@/components/search/SearchArea';
-import { useFrontend } from '@/contexts/FrontendContext';
-
 export default function SearchLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const { query } = useFrontend();
 
     return (
         <div className="container mx-auto px-4 py-16 w-[40%]">

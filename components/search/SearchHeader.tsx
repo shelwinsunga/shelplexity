db29c366f393918b519cc45eacee5d31fa3e996f
Shelwin <shelwinsunga@gmail.com>
2024-07-31 18:37:37 -0500

    style search header

--- components/search/SearchHeader.tsx
+++ components/search/SearchHeader.tsx
@@ -8,8 +8,8 @@ export default function SearchHeader({ query }: { query: string | null }) {
         <>
             {isLongQuery ? (
                 <>
-                    <p className="text-xl font-medium mb-6 pb-2 ">{query}</p>
-                    <Separator />
+                    <p className="text-xl font-medium mb-6 pb-2 ">{query?.slice(0, 150)}...</p>
+                    <Separator className="mb-6" />
                 </>
             ) : (
                 <h1 className="text-3xl font-semibold mb-6">{query}</h1>

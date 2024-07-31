8abb847d4bc83d4482c566a0b303d5387ab01cb1
Shelwin <shelwinsunga@gmail.com>
2024-07-31 15:30:18 -0500

    center page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import { SearchArea } from '@/components/search/SearchArea';
 import { Snail } from 'lucide-react';
 
-export const maxDuration = 30;
-
 export default function Page() {
-
   return (
     <>
-      <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center min-h-screen">
-        <div className="flex flex-col sm:flex-row items-center mb-8 text-center sm:text-left">
-          <Snail className="w-12 h-12 mb-4 sm:mb-0 sm:mr-4" />
-          <h1 className="text-2xl sm:text-3xl font-semibold">Shelplexity</h1>
+      <div className="container mx-auto -mt-16 px-4 py-8 relative flex flex-col items-center justify-center h-screen">
+        <div className="flex items-center mb-8">
+          <Snail className="w-12 h-12 mr-4" />
+          <h1 className="text-3xl font-semibold">Shelplexity</h1>
         </div>
-        <div className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl">
+        <div className="w-full max-w-2xl">
           <SearchArea />
         </div>
       </div>

ba40639fda4b62fb418cb57934f9a8c9a687f069
Shelwin <shelwinsunga@gmail.com>
2024-07-30 23:09:35 -0500

    update

--- contexts/FrontendContext.tsx
+++ contexts/FrontendContext.tsx
@@ -45,18 +45,18 @@ export function FrontendProvider({ children }: { children: React.ReactNode }) {
     router.push(`/search?q=${queryStatus}&newFrontendContextUUID=${newFrontendContextId}`);
 
     const { indexedPath } = await saveFrontendContext(newFrontendContextId, newQuery, 'pending');
-    // setFrontendContextId(newFrontendContextId);
-    // setQuery(newQuery);
+    setFrontendContextId(newFrontendContextId);
+    setQuery(newQuery);
 
-    // setConversation([
-    //   { id: generateId(), role: 'user', display: newQuery },
-    // ]);
-    // const message = await continueConversation(newQuery, indexedPath);
+    setConversation([
+      { id: generateId(), role: 'user', display: newQuery },
+    ]);
+    const message = await continueConversation(newQuery, indexedPath);
 
-    // setConversation((currentConversation: ClientMessage[]) => [
-    //   ...currentConversation,
-    //   message,
-    // ]);
+    setConversation((currentConversation: ClientMessage[]) => [
+      ...currentConversation,
+      message,
+    ]);
 
     // if (message.isComplete) {
     //   for await (const complete of readStreamableValue(message.isComplete)) {

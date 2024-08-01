29447c8e325ffe1d08ee47c8a8a458ddecc8fbe5
Shelwin <shelwinsunga@gmail.com>
2024-07-31 23:43:19 -0500

    feat: add loading skeleton

--- components/search/Loading.tsx
+++ components/search/Loading.tsx
@@ -36,28 +36,27 @@ export default function Loading() {
                             <h2 className="text-2xl font-semibold mb-4">Sources</h2>
                             <div className="flex flex-row gap-2 w-full">
                                 {[...Array(3)].map((_, index) => (
-            
-                                        <Card className="w-1/4 h-32">
-                                            <CardHeader className="py-3 px-3">
-                                                <Skeleton className="h-4 w-3/4" />
-                                            </CardHeader>
-                                            <CardContent className="pb-3 pt-1 px-3">
-                                                <div className="flex items-center">
-                                                    <Skeleton className="h-4 w-4 rounded-full mr-2" />
-                                                    <div className="flex items-center gap-1">
-                                                        <Skeleton className="h-3 w-20" />
-                                                        <Skeleton className="h-3 w-3" />
-                                                        <Skeleton className="h-3 w-3" />
-                                                    </div>
+                                    <Card key={`source-${index}`} className="w-1/4 h-32">
+                                        <CardHeader className="py-3 px-3">
+                                            <Skeleton className="h-4 w-3/4" />
+                                        </CardHeader>
+                                        <CardContent className="pb-3 pt-1 px-3">
+                                            <div className="flex items-center">
+                                                <Skeleton className="h-4 w-4 rounded-full mr-2" />
+                                                <div className="flex items-center gap-1">
+                                                    <Skeleton className="h-3 w-20" />
+                                                    <Skeleton className="h-3 w-3" />
+                                                    <Skeleton className="h-3 w-3" />
                                                 </div>
-                                            </CardContent>
-                                        </Card>
-                                ))}
-                                    <Card className="flex-shrink-0 w-1/4 h-32">
-                                        <CardContent className="flex items-center justify-center h-full">
-                                            <Skeleton className="h-6 w-20" />
+                                            </div>
                                         </CardContent>
                                     </Card>
+                                ))}
+                                <Card className="flex-shrink-0 w-1/4 h-32">
+                                    <CardContent className="flex items-center justify-center h-full">
+                                        <Skeleton className="h-6 w-20" />
+                                    </CardContent>
+                                </Card>
                             </div>
                         </motion.div>
 

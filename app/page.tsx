bfd59f14c025be672f73a3df49a9664aeca2c805
Shelwin <shelwinsunga@gmail.com>
2024-07-31 23:36:14 -0500

    feat: fancier skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
 import { SearchArea } from '@/components/search/SearchArea';
 import { Snail } from 'lucide-react';
 import ImageGallery from '@/components/search/ImageGallery';
+import { SearchPage } from '@/components/search/SearchPage';
 
 export default function Page() {
   return (
     <>
-      <div className="container mx-auto -mt-16 px-4 py-8 relative flex flex-col items-center justify-center h-screen">
-        <div className="flex items-center mb-8">
-          <Snail className="w-12 h-12 mr-4" />
-          <h1 className="text-3xl font-semibold">Shelplexity</h1>
-        </div>
-        <div className="w-full max-w-2xl">
-          <SearchArea />
-        </div>
-      </div>
+      <SearchPage />
     </>
   );
 }

897291446bb9eb6c3dece18c496efb68897ddcc8
Shelwin <shelwinsunga@gmail.com>
2024-08-01 09:58:53 -0500

    feat: skeleton for basic search page

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,20 +3,16 @@ import { Snail } from "lucide-react";
 
 export default function Loading() {
   return (
-    <>
-      <div className="container mx-auto -mt-16 px-4 py-8 relative flex flex-col items-center justify-center h-screen">
-        <div className="flex items-center mb-8">
-          <Snail className="w-12 h-12 mr-4" />
-          <Skeleton className="h-9 w-48" /> {/* Skeleton for the title */}
-        </div>
-        <div className="w-full max-w-2xl">
-          <div className="flex flex-row items-end gap-2 rounded-md border bg-card shadow-md p-4">
-            <Skeleton className="h-[120px] w-full" />{" "}
-            {/* Skeleton for the textarea */}
-            <Skeleton className="h-10 w-10" /> {/* Skeleton for the button */}
-          </div>
+    <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-start sm:justify-center min-h-screen">
+      <div className="flex items-center mb-8">
+        <Snail className="w-8 h-8 sm:w-12 sm:h-12 mr-2 sm:mr-4" />
+        <Skeleton className="h-8 sm:h-9 w-36 sm:w-48" /> {/* Skeleton for the title */}
+      </div>
+      <div className="w-full max-w-2xl sm:mt-0 mt-4">
+        <div className="flex flex-col gap-2 rounded-md border bg-card shadow-md p-4">
+          <Skeleton className="h-[80px] w-full" /> {/* Skeleton for the textarea */}
         </div>
       </div>
-    </>
+    </div>
   );
 }

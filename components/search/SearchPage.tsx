59d42a001a5b4b0b4ddcdff6fe40d26263392add
Shelwin <shelwinsunga@gmail.com>
2024-08-01 01:06:01 -0500

    fix skeleton

--- components/search/SearchPage.tsx
+++ components/search/SearchPage.tsx
@@ -32,8 +32,8 @@ export function SearchPage({ onClose }: { onClose?: () => void }) {
 
   if (isLoading) {
     return (
-      <div className="container mx-auto px-4 py-8 w-full max-w-4xl">
-        <div className="flex flex-col items-start justify-start min-h-screen">
+      <div className="container mx-auto px-4 py-8 sm:py-12 md:py-16 w-full sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%]">
+      <div className="flex flex-col items-start justify-start min-h-screen">
           <Loading />
         </div>
       </div>

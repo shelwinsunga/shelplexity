c5076af674199306f7b1d2d8a7c4b3931a89c5b2
Shelwin <shelwinsunga@gmail.com>
2024-08-02 18:59:32 -0500

    chore: boring cleanup

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,14 +5,12 @@ export default function Loading() {
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-start sm:justify-center min-h-screen">
       <div className="flex items-center mb-8">
-        <Snail className="w-8 h-8 sm:w-12 sm:h-12 mr-2 sm:mr-4" />
+        <Snail className="animate-pulse w-8 h-8 sm:w-12 sm:h-12 mr-2 sm:mr-4" />
         <Skeleton className="h-8 sm:h-9 w-36 sm:w-48" />{" "}
-        {/* Skeleton for the title */}
       </div>
       <div className="w-full max-w-2xl sm:mt-0 mt-4">
         <div className="flex flex-col gap-2 rounded-md border bg-card shadow-md p-4">
           <Skeleton className="h-[80px] w-full" />{" "}
-          {/* Skeleton for the textarea */}
         </div>
       </div>
     </div>
